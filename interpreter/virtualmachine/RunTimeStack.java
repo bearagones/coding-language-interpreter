5e57c18041bc0dfcdea52332eb6a360fd8df8e83
bearagones <baragones@mail.sfsu.edu>
2022-03-02 17:13:09 -0800

    Updated RunTimeStack.java
    - added functions from the PDF
    - completed functions except dump()

--- interpreter/virtualmachine/RunTimeStack.java
+++ interpreter/virtualmachine/RunTimeStack.java
@@ -15,5 +15,42 @@ class RunTimeStack {
         // point of our language, so its frame pointer is 0.
         framePointer.add(0);
     }
+
+    private int lastIndex() {
+        return this.runTimeStack.size() - 1;
+    }
+
+    public void dump() {
+
+    }
+
+    public int peek() {
+        return this.runTimeStack.get(lastIndex());
+    }
+
+    public int push(int valueToPush) {
+        this.runTimeStack.add(valueToPush);
+        return this.peek();
+    }
+
+    public int pop() {
+        return this.runTimeStack.remove(lastIndex());
+    }
+
+    public int store(int offset) {
+        return offset + runTimeStack.get(lastIndex());
+    }
+
+    public int load(int offset) {
+        return framePointer.push(runTimeStack.get(offset));
+    }
+
+    public void newFrameAt(int offset) {
+        framePointer.add(offset);
+    }
+
+    public void popFrame() {
+        framePointer.pop();
+    }
     
 }
