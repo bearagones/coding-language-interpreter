4f2e19f5754010ed05d7c08880391900401509fa
bearagones <baragones@mail.sfsu.edu>
2022-03-04 12:33:09 -0800

    Completed ArgsCode.java

--- interpreter/bytecode/ArgsCode.java
+++ interpreter/bytecode/ArgsCode.java
@@ -1,10 +1,24 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class ArgsCode extends ByteCode {
+    int value;
+
     @Override
     public void init(ArrayList<String> args) {
+        value = Integer.parseInt(args.get(0));
+    }
 
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.newFrameAt(value);
+    }
+
+    @Override
+    public String toString() {
+        return "ARGS " + value;
     }
 }
