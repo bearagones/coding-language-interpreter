5fbc343eee5269a4d6b2c8e5c89b049ddf0d1808
bearagones <baragones@mail.sfsu.edu>
2022-03-04 13:14:48 -0800

    Updated CallCode.java
    - Completed init()
    - Completed toString()
    
    * Need to complete execute()

--- interpreter/bytecode/CallCode.java
+++ interpreter/bytecode/CallCode.java
@@ -1,10 +1,25 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class CallCode extends ByteCode {
+    String value;
+
     @Override
     public void init(ArrayList<String> args) {
+        this.value = args.get(0);
+    }
 
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.pushReturnAddress(vm.getProgramCounter());
+        vm.setProgramCounter(Integer.parseInt());
+    }
+
+    @Override
+    public String toString() {
+        return "CALL " + value;
     }
 }
