7f0bbb5e624188f341a1e6a95cf42f5fad846a97
bearagones <baragones@mail.sfsu.edu>
2022-03-04 13:19:26 -0800

    Updated GotoCode.java
    - Completed init()
    - Completed toString()
    
    * Need to complete execute()

--- interpreter/bytecode/GotoCode.java
+++ interpreter/bytecode/GotoCode.java
@@ -1,10 +1,27 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class GotoCode extends ByteCode {
+    String value;
+
+    // Goto ByteCode has 1 arg -> label to jump to
     @Override
     public void init(ArrayList<String> args) {
+        value = args.get(0);
+    }
 
+    // Goto ByteCode jumps to the specified label
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.setProgramCounter();
+    }
+
+    // Prints out "GOTO " + some label
+    @Override
+    public String toString() {
+        return "GOTO " + value;
     }
 }
