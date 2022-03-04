9b828a2275c3c5f7e4e40f0b19bf061137bb0529
bearagones <baragones@mail.sfsu.edu>
2022-03-03 23:32:54 -0800

    Updated VirtualMachine.java

--- interpreter/virtualmachine/VirtualMachine.java
+++ interpreter/virtualmachine/VirtualMachine.java
@@ -1,5 +1,8 @@
 package interpreter.virtualmachine;
 
+import interpreter.bytecode.ByteCode;
+import interpreter.bytecode.LitCode;
+
 import java.util.Stack;
 
 public class VirtualMachine {
@@ -14,5 +17,8 @@ public class VirtualMachine {
         this.program = program;
     }
 
-    // add executeProgram method
+    public void executeProgram() {
+        ByteCode bc = new LitCode();
+        System.out.println(bc);;
+    }
 }
