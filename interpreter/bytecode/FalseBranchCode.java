609fe5f68b6d3f30f0bc96c752d4f55ec7fa1037
bearagones <baragones@mail.sfsu.edu>
2022-03-04 13:14:12 -0800

    Updated FalseBranchCode.java
    - Completed init()
    - Completed toString()
    
    * Need to complete execute()

--- interpreter/bytecode/FalseBranchCode.java
+++ interpreter/bytecode/FalseBranchCode.java
@@ -1,10 +1,25 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class FalseBranchCode extends ByteCode {
+    String value;
+
+    // FalseBranch ByteCode has 1 arg -> label to jump to
     @Override
     public void init(ArrayList<String> args) {
+        value = args.get(0);
+    }
 
+    @Override
+    public void execute(VirtualMachine vm) {
+        
+    }
+
+    @Override
+    public String toString() {
+        return "FALSEBRANCH " + value;
     }
 }
