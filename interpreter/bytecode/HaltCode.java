bc6ccc588e213e7bf9374435032351f70255b9bc
bearagones <baragones@mail.sfsu.edu>
2022-03-04 13:03:27 -0800

    Completed HaltCode.java

--- interpreter/bytecode/HaltCode.java
+++ interpreter/bytecode/HaltCode.java
@@ -1,10 +1,25 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class HaltCode extends ByteCode {
+
+    // Halt ByteCode has no args
     @Override
     public void init(ArrayList<String> args) {
+    }
 
+    // Halt ByteCode stops the VirtualMachine
+    @Override
+    public void execute(VirtualMachine vm) {
+        vm.haltProgram();
+    }
+
+    // Prints out "HALT"
+    @Override
+    public String toString() {
+        return "HALT";
     }
 }
