5322bd8055cad49ee24974165d930f3402530c4b
bearagones <baragones@mail.sfsu.edu>
2022-03-05 13:10:57 -0800

    Updated FalseBranchCode.java

--- interpreter/bytecode/FalseBranchCode.java
+++ interpreter/bytecode/FalseBranchCode.java
@@ -5,21 +5,25 @@ import interpreter.virtualmachine.VirtualMachine;
 import java.util.ArrayList;
 
 public class FalseBranchCode extends ByteCode {
-    String value;
+    String label;
+    int value;
 
     // FalseBranch ByteCode has 1 arg -> label to jump to
     @Override
     public void init(ArrayList<String> args) {
-        value = args.get(0);
+        this.label = args.get(0);
     }
 
+    // FalseBranch ByteCode executes conditional branching
     @Override
     public void execute(VirtualMachine vm) {
-        
+        if (vm.pop() == 0) {
+            vm.setProgramCounter(Integer.parseInt(label));
+        }
     }
 
     @Override
     public String toString() {
-        return "FALSEBRANCH " + value;
+        return "FALSEBRANCH " + this.label;
     }
 }
