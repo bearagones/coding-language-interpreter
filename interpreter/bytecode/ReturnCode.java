3dbf9744903ab0b1f12fb053336c54fc7d69adef
bearagones <baragones@mail.sfsu.edu>
2022-03-05 14:26:44 -0800

    Updated ReturnCode.java

--- interpreter/bytecode/ReturnCode.java
+++ interpreter/bytecode/ReturnCode.java
@@ -1,10 +1,43 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class ReturnCode extends ByteCode {
+    int programCounter;
+    int temp;
+    String label;
+
+    // Return ByteCode has 0 or 1 arg -> either nothing or a label
     @Override
     public void init(ArrayList<String> args) {
+        if (args.size() > 0) {
+            this.label = args.get(0);
+        }
+    }
 
+    // Return ByteCode returns from functions and puts them in the correct position in the runtime stack
+    @Override
+    public void execute(VirtualMachine vm) {
+        this.programCounter = vm.getReturnAddress();
+        this.temp = vm.pop();
+
+        while (vm.stackSize() > vm.peek()) {
+             vm.pop();
+        }
+
+        vm.pop();
+        vm.push(this.temp);
+        vm.setProgramCounter(this.programCounter);
+    }
+
+    @Override
+    public String toString() {
+        if (this.label != null) {
+            return "RETURN " + this.label;
+        } else {
+            return "RETURN";
+        }
     }
 }
