fc8957b7f174c3348a461451dcf362289c19e2c9
bearagones <baragones@mail.sfsu.edu>
2022-03-05 13:14:04 -0800

    Updated GotoCode.java

--- interpreter/bytecode/GotoCode.java
+++ interpreter/bytecode/GotoCode.java
@@ -5,23 +5,22 @@ import interpreter.virtualmachine.VirtualMachine;
 import java.util.ArrayList;
 
 public class GotoCode extends ByteCode {
-    String value;
+    String label;
 
     // Goto ByteCode has 1 arg -> label to jump to
     @Override
     public void init(ArrayList<String> args) {
-        value = args.get(0);
+        this.label = args.get(0);
     }
 
     // Goto ByteCode jumps to the specified label
     @Override
     public void execute(VirtualMachine vm) {
-        vm.setProgramCounter();
+        vm.setProgramCounter(Integer.parseInt(this.label));
     }
-
-    // Prints out "GOTO " + some label
+    
     @Override
     public String toString() {
-        return "GOTO " + value;
+        return "GOTO " + this.label;
     }
 }
