70f756edbbe6010db68a3878178714f12480d9e2
bearagones <baragones@mail.sfsu.edu>
2022-03-05 14:01:19 -0800

    Updated ReadCode.java

--- interpreter/bytecode/ReadCode.java
+++ interpreter/bytecode/ReadCode.java
@@ -1,10 +1,29 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReadCode extends ByteCode {
+    int userInput;
+
+    // Read ByteCode has 0 args
     @Override
     public void init(ArrayList<String> args) {
+    }
 
+    // Read ByteCode reads input from the user (only integers)
+    @Override
+    public void execute(VirtualMachine vm) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        this.userInput = scanner.nextInt();
+        vm.push(this.userInput);
+    }
+
+    @Override
+    public String toString() {
+        return "READ";
     }
 }
