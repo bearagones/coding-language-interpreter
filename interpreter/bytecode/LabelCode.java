5327e7814de49b34bb93390ca5c7a0fee8292476
bearagones <baragones@mail.sfsu.edu>
2022-03-05 13:17:54 -0800

    Updated LabelCode.java

--- interpreter/bytecode/LabelCode.java
+++ interpreter/bytecode/LabelCode.java
@@ -1,10 +1,25 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class LabelCode extends ByteCode {
+    String label;
+
+    // Label ByteCode has 1 arg -> a label to denote a location in the program
     @Override
     public void init(ArrayList<String> args) {
+        this.label = args.get(0);
+    }
 
+    // Label ByteCode has no functionality
+    @Override
+    public void execute(VirtualMachine vm) {
+    }
+
+    @Override
+    public String toString() {
+        return "LABEL " + this.label;
     }
 }
