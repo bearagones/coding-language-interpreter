1bbae0a5cafaab9e099f6499bd9cc4e7b1fd7ea6
bearagones <baragones@mail.sfsu.edu>
2022-03-05 13:52:29 -0800

    Updated PopCode.java

--- interpreter/bytecode/PopCode.java
+++ interpreter/bytecode/PopCode.java
@@ -5,25 +5,24 @@ import interpreter.virtualmachine.VirtualMachine;
 import java.util.ArrayList;
 
 public class PopCode extends ByteCode {
-    int value;
+    int numValue;
 
     // Pop ByteCode has 1 arg -> # of values to remove from runtime stack
     @Override
     public void init(ArrayList<String> args) {
-        value = Integer.parseInt(args.get(0));
+        this.numValue = Integer.parseInt(args.get(0));
     }
 
     // Pop ByteCode removes the number of values from the runtime stack
     @Override
     public void execute(VirtualMachine vm) {
-        for (int i = 1; i < value; i++) {
+        for (int i = 1; i < this.numValue; i++) {
             vm.pop();
         }
     }
 
-    // Prints out "POP " + some integer
     @Override
     public String toString() {
-        return "POP " + value;
+        return "POP " + this.numValue;
     }
 }
