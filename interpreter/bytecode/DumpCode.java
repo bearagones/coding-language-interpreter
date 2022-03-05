13bee60324d3710f5bc2726d20cbe05481fcf3ba
bearagones <baragones@mail.sfsu.edu>
2022-03-05 12:44:43 -0800

    Updated DumpCode.java

--- interpreter/bytecode/DumpCode.java
+++ interpreter/bytecode/DumpCode.java
@@ -5,18 +5,18 @@ import interpreter.virtualmachine.VirtualMachine;
 import java.util.ArrayList;
 
 public class DumpCode extends ByteCode {
-    String value;
+    String dumpStatus;
 
     // Dump ByteCode has 1 arg -> "ON" or "OFF"
     @Override
     public void init(ArrayList<String> args) {
-        value = args.get(0);
+        this.dumpStatus = args.get(0);
     }
 
     // Dump ByteCode requests to turn dumping ON or OFF
     @Override
     public void execute(VirtualMachine vm) {
-        switch (value) {
+        switch (this.dumpStatus) {
             case "ON" -> vm.dumpOn();
             case "OFF" -> vm.dumpOff();
         }
