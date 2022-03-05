e36a8a704ccb9287d8cdc0778a91f76c2a41033b
bearagones <baragones@mail.sfsu.edu>
2022-03-05 12:30:55 -0800

    Updated BopCode.java

--- interpreter/bytecode/BopCode.java
+++ interpreter/bytecode/BopCode.java
@@ -6,18 +6,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BopCode extends ByteCode {
-    String value;
+    String operation;
     int firstOperand;
     int secondOperand;
 
+    // Bop ByteCode has 1 arg -> an operation in the form of a string
     @Override
     public void init(ArrayList<String> args) {
-        this.value = args.get(0);
+        this.operation = args.get(0);
     }
 
+    // Bop ByteCode pops two values from the stack and performs an operation on them
     @Override
     public void execute(VirtualMachine vm) {
-
         this.firstOperand = vm.pop();
         this.secondOperand = vm.pop();
 
@@ -35,11 +36,11 @@ public class BopCode extends ByteCode {
         bopMap.put("|", this.firstOperand | this.secondOperand);
         bopMap.put("&", this.firstOperand & this.secondOperand);
 
-        vm.push(bopMap.get(this.value));
+        vm.push(bopMap.get(this.operation));
     }
 
     @Override
     public String toString() {
-        return "BOP " + value;
+        return "BOP " + operation;
     }
 }
