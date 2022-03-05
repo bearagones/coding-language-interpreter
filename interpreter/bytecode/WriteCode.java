74e2bf9f4c17699fc94450c6144cda63ee71965d
bearagones <baragones@mail.sfsu.edu>
2022-03-04 21:35:56 -0800

    Completed WriteCode.java

--- interpreter/bytecode/WriteCode.java
+++ interpreter/bytecode/WriteCode.java
@@ -1,10 +1,24 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class WriteCode extends ByteCode {
+
+    // Write ByteCode has 0 args
     @Override
     public void init(ArrayList<String> args) {
+    }
 
+    // Write ByteCode prints only the top of the runtime stack
+    @Override
+    public void execute(VirtualMachine vm) {
+        System.out.println(vm.peek());
+    }
+
+    @Override
+    public String toString() {
+        return "WRITE";
     }
 }
