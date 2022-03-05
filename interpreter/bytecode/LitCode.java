4a9d1af000f1c94e98e1dd08869672546c64d55d
bearagones <baragones@mail.sfsu.edu>
2022-03-05 13:19:51 -0800

    Updated LitCode.java

--- interpreter/bytecode/LitCode.java
+++ interpreter/bytecode/LitCode.java
@@ -1,11 +1,14 @@
 package interpreter.bytecode;
 
+import interpreter.virtualmachine.VirtualMachine;
+
 import java.util.ArrayList;
 
 public class LitCode extends ByteCode {
     int value;
     String id;
 
+    // Lit ByteCode has 1 or 2 args -> a value and (if present) an id of the value
     @Override
     public void init(ArrayList<String> args) {
         this.value = Integer.parseInt(args.get(0));
@@ -14,6 +17,16 @@ public class LitCode extends ByteCode {
         }
     }
 
+    // Lit ByteCode pushes only 1 value on top of the runtime stack
+    @Override
+    public void execute(VirtualMachine vm) {
+        if (id.equals("")) {
+            vm.push(value);
+        } else {
+            vm.push(0);
+        }
+    }
+    
     @Override
     public String toString() {
         String base = "LIT " + value;
