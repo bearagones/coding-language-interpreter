bab592b75c1675e7cb66e0556425dd0c91b80245
bearagones <baragones@mail.sfsu.edu>
2022-03-11 15:52:50 -0800

    Updated VirtualMachine.java

--- interpreter/virtualmachine/VirtualMachine.java
+++ interpreter/virtualmachine/VirtualMachine.java
@@ -1,8 +1,8 @@
 package interpreter.virtualmachine;
 
-import interpreter.bytecode.ByteCode;
-import interpreter.bytecode.LitCode;
+import interpreter.bytecode.*;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class VirtualMachine {
@@ -12,13 +12,100 @@ public class VirtualMachine {
     private Program        program;
     private int            programCounter;
     private boolean        isRunning;
+    private boolean        dumpStatus;
 
     public VirtualMachine(Program program) {
         this.program = program;
     }
 
     public void executeProgram() {
-        ByteCode bc = new LitCode();
-        System.out.println(bc);;
+        programCounter = 0;
+        runTimeStack = new RunTimeStack();
+        returnAddress = new Stack<>();
+        isRunning = true;
+        dumpStatus = true;
+
+        while (isRunning) {
+            ByteCode code = program.getCode(programCounter);
+            code.execute(this);
+            dump(code);
+            programCounter++;
+        }
     }
+
+    public void haltProgram() {
+        isRunning = false;
+    }
+
+    public void dump(ByteCode code) {
+        if (dumpStatus && !(code instanceof DumpCode) && !(code instanceof HaltCode)) {
+            System.out.println(code);
+        }
+        this.runTimeStack.dump();
+    }
+
+    public void dumpOn() {
+        dumpStatus = true;
+    }
+
+    public void dumpOff() {
+        dumpStatus = false;
+    }
+
+    public int peek() {
+        return this.runTimeStack.peek();
+    }
+
+    public void push(int valueToPush) {
+        this.runTimeStack.push(valueToPush);
+    }
+
+    public int pop() {
+        return this.runTimeStack.pop();
+    }
+
+    public int store(int offset) {
+        return this.runTimeStack.store(offset);
+    }
+
+    public int load(int offset) {
+        return this.runTimeStack.load(offset);
+    }
+
+    public void newFrameAt(int offset) {
+        this.runTimeStack.newFrameAt(offset);
+    }
+
+    public void popFrame() {
+        this.runTimeStack.popFrame();
+    }
+
+    public int popReturnAddress() {
+        return this.returnAddress.pop();
+    }
+
+    public void setProgramCounter(int index) {
+        programCounter = index;
+    }
+
+    public int stackSize() {
+        return this.runTimeStack.stackSize();
+    }
+
+    public int frameSize() {
+        return this.runTimeStack.frameSize();
+    }
+
+    public void save() {
+        returnAddress.push(programCounter);
+    }
+
+    public void jumpTo(int index) {
+        programCounter = index;
+    }
+
+    public ArrayList<Integer> frameElements() {
+        return this.runTimeStack.frameElements();
+    }
+
 }
