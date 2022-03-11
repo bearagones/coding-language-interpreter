24b16e97e88d234c837dc1c531c4dea8292ca5c2
bearagones <baragones@mail.sfsu.edu>
2022-03-11 15:52:44 -0800

    Updated RunTimeStack.java

--- interpreter/virtualmachine/RunTimeStack.java
+++ interpreter/virtualmachine/RunTimeStack.java
@@ -5,8 +5,8 @@ import java.util.Stack;
 
 class RunTimeStack {
 
-    private ArrayList<Integer> runTimeStack;
-    private Stack<Integer>     framePointer;
+    private final ArrayList<Integer> runTimeStack;
+    private final Stack<Integer>     framePointer;
 
     public RunTimeStack() {
         runTimeStack = new ArrayList<>();
@@ -21,7 +21,26 @@ class RunTimeStack {
     }
 
     public void dump() {
-        
+        String base = "[";
+        int value;
+        for (int i = 0; i < framePointer.size(); i++) {
+            if (i > 0) {
+                base += "] [";
+            }
+            if (i != framePointer.size() - 1) {
+                value = framePointer.get(i + 1);
+            } else {
+                value = framePointer.size();
+            }
+            for (int j = framePointer.get(i); j < value; j++) {
+                if (j == value - 1) {
+                    base += runTimeStack.get(j);
+                } else {
+                    base += runTimeStack.get(j) + ",";
+                }
+            }
+        }
+        System.out.println(base + "]");
     }
 
     public int peek() {
@@ -38,19 +57,45 @@ class RunTimeStack {
     }
 
     public int store(int offset) {
-        return offset + runTimeStack.get(lastIndex());
+        int temp = runTimeStack.get(lastIndex());
+        runTimeStack.remove(lastIndex());
+        runTimeStack.set(framePointer.peek() + offset, temp);
+        return temp;
     }
 
     public int load(int offset) {
-        return framePointer.push(runTimeStack.get(offset));
+        int currentFrame = framePointer.peek();
+        int value = runTimeStack.get(currentFrame + offset);
+        runTimeStack.add(value);
+        return value;
     }
 
     public void newFrameAt(int offset) {
-        framePointer.add(offset);
+        framePointer.push(runTimeStack.size() - offset);
     }
 
     public void popFrame() {
+        int currentFrameIndex = framePointer.peek();
+        for (int i = lastIndex(); i >= currentFrameIndex; i--) {
+            runTimeStack.remove(i);
+        }
         framePointer.pop();
     }
-    
+
+    public int stackSize() {
+        return this.runTimeStack.size();
+    }
+
+    public int frameSize() {
+        return stackSize() - framePointer.peek();
+    }
+
+    public ArrayList<Integer> frameElements() {
+        int frameIndex = framePointer.peek();
+        ArrayList<Integer> frameElements = new ArrayList<>();
+        for (int i = frameIndex; i < stackSize(); i++) {
+            frameElements.add(runTimeStack.get(i));
+        }
+        return frameElements;
+    }
 }
